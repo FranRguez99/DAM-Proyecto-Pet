6b1178945a7328a8d6e8c7e8e3e3f1f1fdb63ed2
Julike Manji <julianerrasti99@gmail.com>
2023-03-07 18:03:49 +0100

    login hecho

--- app/src/main/java/es/duarry/loginbasico/LoginActivity.kt
+++ app/src/main/java/es/duarry/loginbasico/LoginActivity.kt
@@ -1,13 +1,25 @@
 package es.duarry.loginbasico
 
+import android.content.ContentValues
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import es.duarry.loginbasico.databinding.LoginActivityBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 
 class LoginActivity : AppCompatActivity() {
@@ -16,23 +28,29 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var contra: String
     private lateinit var bd: BaseDatos
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
         binding = LoginActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
         // inicio bd y sharedPreferences
         bd = BaseDatos(this)
         sharedPreferences = this.getSharedPreferences("userdetails", MODE_PRIVATE)
 
         //Datos iniciales base datos
-        datosIniciales()
+   /*     datosIniciales()
 
         // Recuerdo usuario
-        userGuardado()
+        userGuardado()*/
 
+        binding.strRegistroPregunta.setOnClickListener(){
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
         // login de user
         binding.login.setOnClickListener {
 
@@ -42,13 +60,51 @@ class LoginActivity : AppCompatActivity() {
             if (TextUtils.isEmpty(usuario) || TextUtils.isEmpty(contra)) {
                 Snackbar.make(binding.textView2, "Usuario y/o contraseña incorrectos", Snackbar.LENGTH_SHORT).show()
             } else {
-                checkUsuario()
+                autentificacionLogin()
             }
         }
     }
+    private fun actualizaUI(user: FirebaseUser?) {
+        if (user != null) {
+            // El usuario está autenticado, mostrar la vista para usuario autenticado
+            // por ejemplo, mostrar un mensaje de bienvenida y un botón de cerrar sesión
 
+        } else {
+            // El usuario no está autenticado, mostrar la vista para usuario no autenticado
+            // por ejemplo, mostrar un formulario de inicio de sesión y ocultar botón de cerrar sesión
+        }
+    }
+
+    private fun autentificacionLogin(){
+
+        val usuario = binding.user.editText?.text.toString()
+        val contrasena = binding.pasword.editText?.text.toString()
+        val intent = Intent(this, RegisAnimalActivity::class.java)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val result = auth.signInWithEmailAndPassword(usuario, contrasena).await()
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(ContentValues.TAG, "Login: Correcto")
+                val user = result.user
+
+                withContext(Dispatchers.Main) {
+                    startActivity(intent)
+                    actualizaUI(user)
+                }
+            } catch (e: Exception) {
+                // If sign in fails, display a message to the user.
+                Log.w(ContentValues.TAG, "Login: Erróneo", e)
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(baseContext, "Autentificación errónea", Toast.LENGTH_SHORT).show()
+                    actualizaUI(null)
+                }
+            }
+        }
+
+    }
     // Confirmar que el usuario está registrado
-    fun checkUsuario(){
+   /* fun checkUsuario(){
         val checkUsuario = bd.verificaUsuario(usuario, contra)
         if (checkUsuario) {
             val intent = Intent(this, RegisAnimalActivity::class.java)
@@ -84,5 +140,5 @@ class LoginActivity : AppCompatActivity() {
         contra = sharedPreferences.getString("conGuardado","").toString()
         binding.user.editText?.setText(usuario)
         binding.pasword.editText?.setText(contra)
-    }
+    }*/
 }
--- app/src/main/java/es/duarry/loginbasico/RegisterActivity.kt
+++ app/src/main/java/es/duarry/loginbasico/RegisterActivity.kt
@@ -1,17 +1,74 @@
 package es.duarry.loginbasico
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import es.duarry.loginbasico.databinding.ActivityRegisterBinding
 import es.duarry.loginbasico.databinding.LoginActivityBinding
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding:ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.btregistro.setOnClickListener(){
+            registerUser()
+        }
     }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            // El usuario está autenticado, mostrar la vista para usuario autenticado
+            // por ejemplo, mostrar un mensaje de bienvenida y un botón de cerrar sesión
+
+        } else {
+            // El usuario no está autenticado, mostrar la vista para usuario no autenticado
+            // por ejemplo, mostrar un formulario de inicio de sesión y ocultar botón de cerrar sesión
+        }
+    }
+    private fun registerUser() {
+        val email = binding.user.editText?.text.toString()
+        val password = binding.pasword.editText?.text.toString()
+        val confirmPassword = binding.pasword2.editText?.text.toString()
+
+        if (password != confirmPassword) {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val result = auth.createUserWithEmailAndPassword(email, password).await()
+                // Registration success, update UI with the signed-in user's information
+                Log.d(TAG, "createUserWithEmailAndPassword:success")
+                val user = result.user
+                withContext(Dispatchers.Main) {
+                    updateUI(user)
+                }
+            } catch (e: Exception) {
+                // If registration fails, display a message to the user.
+                Log.w(TAG, "createUserWithEmailAndPassword:failure", e)
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(baseContext, "Registration failed.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        }
+    }
+
 }
