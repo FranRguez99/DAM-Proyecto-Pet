5088fdf49bc196f801e89df71f9a53cb19e3bbfa
Eduardo <eduarry@hotmail.com>
2023-03-08 13:19:45 +0100

    Consultar todos hecho

--- app/src/main/java/es/duarry/loginbasico/RegisAnimalActivity.kt
+++ app/src/main/java/es/duarry/loginbasico/RegisAnimalActivity.kt
@@ -38,14 +38,12 @@ class RegisAnimalActivity : AppCompatActivity() {
 
         binding.btnAlta.setOnClickListener {
             agregarAnimal()
-
-
+            limpiaCampos()
         }
 
         binding.btnModifica.setOnClickListener {
             modificaAnimal()
             limpiaCampos()
-
         }
 
         binding.btnBorra.setOnClickListener {
@@ -53,9 +51,10 @@ class RegisAnimalActivity : AppCompatActivity() {
                 eliminaAnimal(animalId!!)
                 limpiaCampos()
             } else {
-                Toast.makeText(this, "No se puede eliminar el animal: no se ha seleccionado ningún animal", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "No se puede eliminar el animal: no se ha seleccionado ningún animal.", Toast.LENGTH_SHORT).show()
             }
         }
+
         binding.btnConsulta.setOnClickListener {
             val cod = binding.codAnimal.text.toString()
             val animal = bd.consultaAnimal(cod)
@@ -87,7 +86,6 @@ class RegisAnimalActivity : AppCompatActivity() {
             val intent = Intent(this, ListadoActivity::class.java)
             intent.putExtra("usuario", usuario)
             startActivity(intent)
-
         }
 
         FirebaseApp.initializeApp(this)
@@ -214,6 +212,31 @@ class RegisAnimalActivity : AppCompatActivity() {
             .addOnFailureListener {
                 Toast.makeText(this, "Error al eliminar el animal", Toast.LENGTH_SHORT).show()
             }
+    }
+
+    private fun consultarAnimales() {
+        database.child("animales").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val animales = ArrayList<Animales>()
+                for (animalSnapshot in dataSnapshot.children) {
+                    val animal = animalSnapshot.getValue(Animales::class.java)
+                    if (animal != null) {
+                        animales.add(animal)
+                    }
+                }
+                //aquí puedes hacer lo que quieras con la lista de animales obtenida
+                //por ejemplo, mostrarla en un RecyclerView o en una lista
+                //de elementos en una vista de lista
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(
+                    this@RegisAnimalActivity,
+                    "Error al consultar los animales",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 
     fun limpiaCampos() {
