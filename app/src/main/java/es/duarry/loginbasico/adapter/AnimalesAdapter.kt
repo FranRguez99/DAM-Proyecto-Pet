7c1cc05e1a28fe71e06913412b456abe7a916322
Eduardo <eduarry@hotmail.com>
2023-03-08 21:06:31 +0100

    Al pulsar CardView te lleva a activity_ver_animal.xml

--- app/src/main/java/es/duarry/loginbasico/adapter/AnimalesAdapter.kt
+++ app/src/main/java/es/duarry/loginbasico/adapter/AnimalesAdapter.kt
@@ -1,16 +1,17 @@
 package es.duarry.loginbasico.adapter
 
+import AnimalesViewHolder
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import es.duarry.loginbasico.Animales
 import es.duarry.loginbasico.R
 
-class animalesAdapter(private val listaAnimales: List<Animales>) : RecyclerView.Adapter<AnimalesViewHolder>(){
+class AnimalesAdapter(private val listaAnimales: List<Animales>) : RecyclerView.Adapter<AnimalesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalesViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        return AnimalesViewHolder(layoutInflater.inflate(R.layout.item_animales, parent , false))
+        return AnimalesViewHolder(layoutInflater.inflate(R.layout.item_animales, parent, false))
     }
 
     override fun onBindViewHolder(holder: AnimalesViewHolder, position: Int) {
@@ -19,5 +20,4 @@ class animalesAdapter(private val listaAnimales: List<Animales>) : RecyclerView.
     }
 
     override fun getItemCount(): Int = listaAnimales.size
-
 }
